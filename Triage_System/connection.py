28e8810dcf15ee7c99336843dd67bc42c48fe4e0
sgunthrope11 <sgunthrope11@gmail.com>
2026-04-02 22:22:04 -0400

    week3: remove test block from connection.py, add env support

--- Triage_System/connection.py
+++ Triage_System/connection.py
@@ -1,17 +1,14 @@
 import pyodbc
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_connection():
     conn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};'
-        'SERVER=localhost;'
-        'DATABASE=Chatbot_ticketing_SYS;'  # replace with your actual DB name
-        'Trusted_Connection=yes;'
+        f'DRIVER={{ODBC Driver 17 for SQL Server}};'
+        f'SERVER={os.environ.get("DB_SERVER", "localhost")};'
+        f'DATABASE={os.environ.get("DB_NAME", "Chatbot_Ticketing_SYS")};'
+        f'Trusted_Connection=yes;'
     )
     return conn
-
-try:
-    conn = get_connection()
-    print("Connection successful!")
-    conn.close()
-except Exception as e:
-    print("Connection failed:", e)
